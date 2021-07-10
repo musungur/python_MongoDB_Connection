48f9cc8cbf6e106c89ad2fb8f2fd622c1b126dbd
Robert Musungu <robertmusungu3@gmail.com>
2021-07-10 07:27:36 +0300

    pycode connection to MongoDB

---
+++ mongodb_connection.py
@@ -0,0 +1,15 @@
+# connection to mongodb
+
+import pymongo
+import os
+try:
+    mongo = pymongo.MongoClient(
+            host = "127.0.0.1",
+            port = 12017,
+            serverSelectionTimeoutTM = 1000
+            )
+    db = mongo.students
+    print(f"returned {db.status_code} success")
+    info = mongo.server_info()
+except Exception as e:
+    print(e)
